080424c9fcba8b6995d6ee1313da9d1110d6cd6d
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2022-01-06 12:47:11 +0300

    add missing queries to contract detail endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Op } = require("sequelize");
 const { sequelize } = require("./model");
 const { getProfile } = require("./middleware/getProfile");
 const app = express();
@@ -8,13 +9,21 @@ app.set("sequelize", sequelize);
 app.set("models", sequelize.models);
 
 /**
- * FIX ME!
  * @returns contract by id
  */
 app.get("/contracts/:id", getProfile, async (req, res) => {
   const { Contract } = req.app.get("models");
-  const { id } = req.params;
-  const contract = await Contract.findOne({ where: { id } });
+  const {
+    params: { id },
+    profile: { id: profileId },
+  } = req;
+
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+    },
+  });
   if (!contract) return res.status(404).end();
   res.json(contract);
 });
